0eaa7580994995502b941500525b85838eb8c88d
Ivo van Dongen <info@ivovandongen.nl>
2018-09-16 12:41:25 +0300

    cleanup tools

--- src/tools.cpp
+++ src/tools.cpp
@@ -28,28 +28,3 @@ Eigen::VectorXd Tools::CalculateRMSE(const std::vector<Eigen::VectorXd> &estimat
     rmse /= estimations.size();
     return rmse.array().sqrt();
 }
-
-Eigen::MatrixXd Tools::CalculateJacobian(const Eigen::VectorXd &x_state) {
-    Eigen::MatrixXd Hj(3, 4);
-
-    double px = x_state(0);
-    double py = x_state(1);
-    double vx = x_state(2);
-    double vy = x_state(3);
-
-    double c1 = px * px + py * py;
-    double c2 = sqrt(c1);
-
-    // check division by zero
-    if (c1 < 0.0001) {
-        std::cout << "Jacobian - error division by zero" << std::endl;
-        return Hj;
-    }
-
-    // compute the Jacobian matrix
-    Hj.row(0) << px / c2, py / c2, 0, 0;
-    Hj.row(1) << -py / (c2 * c2), px / (c2 * c2), 0, 0;
-    Hj.row(2) << py * (vx * py - vy * px) / (c2 * c2 * c2), px * (vy * px - vx * py) / (c2 * c2 * c2), px / c2, py / c2;
-
-    return Hj;
-}
--- src/tools.hpp
+++ src/tools.hpp
@@ -21,9 +21,4 @@ public:
      */
     static Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations,
                                          const std::vector<Eigen::VectorXd> &ground_truth);
-
-    /**
-     * A helper method to calculate Jacobians.
-     */
-    static Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd &x_state);
 };
